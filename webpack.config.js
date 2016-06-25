7b4448f368f601f289faaaf37b7d71606775d351
Zofia Korcz <zofia.korcz@guardian.co.uk>
2016-06-25 21:26:30 +0100

    small configuration changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,20 @@ var webpack = require('webpack');
 module.exports = {
 	context: __dirname, //current folder as the reference to the other paths
 	entry: {
-		game: './game.js' //entry point for building scripts
+		game: './game.js' //which file should be loaded on the page
 	},
 	output: {
-		path: path.resolve('./dist'), //save result in 'dist' folder
-		filename: './game.js',
+		path: path.resolve('./dist'), //where the compiled JavaScript file should be saved
+		filename: './game.js', //name of the compiled JavaScript file
 		publicPath: '/mastermind_webpack/'
 	},
 	module: {
 		loaders: [
-			{ //transpile ES2015 with JSX into ES5
-				test: /\.js?$/,
+			{
+				test: /\.js?$/, //translate and compile ES6 with JSX into ES5
 				exclude: /node_modules/,
 				loader: 'babel',
-				query: {
+				query: { //query configuration passed to the loader
 					presets: ['react', 'es2015']
 				}
 			}
